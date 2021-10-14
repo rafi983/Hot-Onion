94017974437c5919faddbadefba10dafcbc78cb8
rafi983 <rafiirfan211@gmail.com>
2021-10-14 22:27:10 +0600

    commit

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -1,25 +1,40 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
+import FoodsItem from "../../FoodsItem";
 import Category from "../Category/Category";
 
 const Categories = () => {
-  const [foods, setFoods] = useState([]);
+  const [foods, setFoods] = useState(FoodsItem);
 
-  useEffect(() => {
-    fetch(
-      "https://raw.githubusercontent.com/rafi983/Hot-Onion/main/public/foods.json"
-    )
-      .then((res) => res.json())
-      .then((data) => setFoods(data));
-  }, []);
+  const filterCategory = (curCatg) => {
+    const updatedCategory = FoodsItem.filter((fd) => {
+      return fd.category === curCatg;
+    });
+
+    setFoods(updatedCategory);
+  };
 
   return (
-    <div className="container">
-      <div className="row d-flex justiy-content-center">
-        {foods.map((food) => (
-          <Category food={food}></Category>
-        ))}
+    <>
+      <div className="categories d-flex justify-content-center mt-5">
+        <h5 onClick={() => setFoods(FoodsItem)}>All</h5>
+        <h5 className="ms-4" onClick={() => filterCategory("breakfast")}>
+          Breakfast
+        </h5>
+        <h5 className="ms-4" onClick={() => filterCategory("lunch")}>
+          Lunch
+        </h5>
+        <h5 className="ms-4" onClick={() => filterCategory("dinner")}>
+          Dinner
+        </h5>
       </div>
-    </div>
+      <div className="container">
+        <div className="row d-flex justiy-content-center">
+          {foods.map((food) => (
+            <Category key={food.image} food={food}></Category>
+          ))}
+        </div>
+      </div>
+    </>
   );
 };
 
