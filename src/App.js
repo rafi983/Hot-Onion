bdf192849af4ef05493acb536b307441428d6e92
rafi983 <rafiirfan211@gmail.com>
2021-10-14 19:59:27 +0600

    home-sec-added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Home from "./components/Home/Home";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Home />
     </div>
   );
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,27 +1,54 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import logo from "../../more-images/logo2.png";
+import "./Home.css";
 
 const Home = () => {
-  const [foods, setFoods] = useState([]);
-
-  useEffect(() => {
-    fetch(
-      "https://raw.githubusercontent.com/rafi983/Hot-Onion/main/public/foods.json"
-    )
-      .then((res) => res.json())
-      .then((data) => setFoods(data));
-  }, []);
-
   return (
-    <div>
-      {foods.map((fd) => (
-        <>
-          <h1>{fd.title}</h1>
-          <p>{fd.desc}</p>
-          <p>{fd.price}</p>
-          <img src={fd.image} alt="" />
-        </>
-      ))}
-    </div>
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8">
+            <div className="logo">
+              <img src={logo} className="logo mt-2" alt="" />
+            </div>
+          </div>
+
+          <div className="col-md-4">
+            <div className="header-content d-flex mt-2">
+              <h4 className="mt-2">Dashboard</h4>
+              <button className="btn btn-regular ms-3 rounded-pill px-4">
+                Sign In
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="banner">
+        <div className="banner-content">
+          <h1 className="mb-4">Best food waiting for your belly</h1>
+
+          <div className="search">
+            <div className="input-group w-100">
+              <input
+                id="search-field"
+                type="text"
+                className="form-control rounded-pill p-3"
+                placeholder="search your favourite food..."
+              />
+              <div className="input-group-append">
+                <button
+                  type="button"
+                  className="btn btn-search rounded-pill px-5 py-3"
+                >
+                  Search
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
