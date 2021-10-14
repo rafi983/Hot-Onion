3fd0d05c7f1dc91f13f88b56b61d18cc5c660551
rafi983 <rafiirfan211@gmail.com>
2021-10-15 00:05:39 +0600

    added

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import FoodsItem from "../../FoodsItem";
 import Category from "../Category/Category";
+import "./Categories.css";
 
 const Categories = () => {
   const [foods, setFoods] = useState(FoodsItem);
@@ -17,16 +18,11 @@ const Categories = () => {
     <>
       <div className="categories d-flex justify-content-center mt-5">
         <h5 onClick={() => setFoods(FoodsItem)}>All</h5>
-        <h5 className="ms-4" onClick={() => filterCategory("breakfast")}>
-          Breakfast
-        </h5>
-        <h5 className="ms-4" onClick={() => filterCategory("lunch")}>
-          Lunch
-        </h5>
-        <h5 className="ms-4" onClick={() => filterCategory("dinner")}>
-          Dinner
-        </h5>
+        <h5 onClick={() => filterCategory("breakfast")}>Breakfast</h5>
+        <h5 onClick={() => filterCategory("lunch")}>Lunch</h5>
+        <h5 onClick={() => filterCategory("dinner")}>Dinner</h5>
       </div>
+
       <div className="container">
         <div className="row d-flex justiy-content-center">
           {foods.map((food) => (
