360159aaede6a7d7d96385ed7d2ccb7f8c058d0c
rafi983 <rafiirfan211@gmail.com>
2021-10-14 18:09:38 +0600

    first commit

---
+++ src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  // const [foods,setFoods] = =useState([])
+  return (
+    <div>
+      <h1>home</h1>
+    </div>
+  );
+};
+
+export default Home;
